0a9161d5b91397fd285554115fdc7c15d191c72c
i5ting <i5ting@126.com>
2015-11-23 17:29:56 +0800

    init

---
+++ install.sh
@@ -0,0 +1,16 @@
+#! /bin/bash
+
+curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.29.0/install.sh | bash
+
+echo "\n" >> ~/.zshrc
+echo "export NVM_DIR="/home/coding/.nvm"" >> ~/.zshrc
+echo "[ -s "$NVM_DIR/nvm.sh" ] && . "$NVM_DIR/nvm.sh"" >> ~/.zshrc
+echo "\n" >> ~/.zshrc
+
+source /home/coding/.zshrc 
+
+nvm install 4.2 && nvm alias default 4.2 && node -v
+
+npm i -g nrm  && nrm use taobao
+
+nrm ls 
