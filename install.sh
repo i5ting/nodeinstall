bda9842a013d858a0616f6c2d8f747785e832093
i5ting <i5ting@126.com>
2015-11-27 12:05:28 +0800

    doc

--- install.sh
+++ install.sh
@@ -2,11 +2,7 @@
 
 curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.29.0/install.sh | bash
 
-SOURCE_STR="\nexport NVM_DIR=\"$NVM_DIR\"\n[ -s \"\$NVM_DIR/nvm.sh\" ] && . \"\$NVM_DIR/nvm.sh\"  # This loads nvm"
-
-printf "$SOURCE_STR\n" >> ~/.zshrc 
-
-source ~/.zshrc 
+source ~/.bashrc 
 
 nvm install 4.2
 nvm alias default 4.2
